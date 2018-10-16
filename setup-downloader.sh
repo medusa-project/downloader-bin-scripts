8e21adeddb03f78cfaf071f9e60ebee666f7e905
Howard Ding <hading2@gmail.com>
2018-10-16 10:07:09 -0500

    Add stubs for setup scripts and the current monit file

---
+++ setup-downloader.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#This is to set up the things the local user can install, like
+#getting source packages, rbenv, etc.
---
+++ setup-system.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#This is to set up things like yum packages needed an so forth, so will
+#need to be run with sudo
