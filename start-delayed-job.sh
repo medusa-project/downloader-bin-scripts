f39c0cc60a3cd85ae10e7df39f200175a3c04cac
Gen Schmitt <gschmitt@illinois.edu>
2025-01-27 13:45:25 -0700

    Updated start delayed job script to capture and create pid file

--- start-delayed-job.sh
+++ start-delayed-job.sh
@@ -5,6 +5,7 @@ source $HOME/bin/set-vars.sh
 cd $RAILS_HOME
 bundle install
 bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR start
-
+sleep 10
+awk 'END{print}' log/delayed_job_$RAILS_ENV.log | cut -d "#" -f 2 | cut -d "]" -f 1 > $PID_DIR/delayed_job.pid
 
 
