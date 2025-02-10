94e73e176c422830d6c0d10362f691667c1f9bfa
Gen Schmitt <gschmitt@illinois.edu>
2025-02-10 12:24:50 -0700

    Updated mountpoint mount script to start in background

--- mountpoint-mount.sh
+++ mountpoint-mount.sh
@@ -6,5 +6,5 @@ mkdir -p $MOUNTPOINT_DIR
 mkdir -p $MOUNTPOINT_PIDDIR
 
 mountpoint -q $MOUNTPOINT_DIR && fusermount -uz $MOUNTPOINT_DIR
-mount-s3 --read-only $MOUNTPOINT_BUCKET $MOUNTPOINT_DIR
+mount-s3 --read-only $MOUNTPOINT_BUCKET $MOUNTPOINT_DIR &
 echo $! > $MOUNTPOINT_PIDFILE
