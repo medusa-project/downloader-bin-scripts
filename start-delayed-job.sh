60e43d782fcd71240a506aafdb6922c148cca00f
Gen Schmitt <gschmitt@illinois.edu>
2025-02-13 13:19:26 -0700

    Updated start delayed pgrep to use environment var

--- start-delayed-job.sh
+++ start-delayed-job.sh
@@ -6,6 +6,6 @@ cd $RAILS_HOME
 bundle install
 bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR start &
 sleep 2
-pgrep -f delayed_job > $PID_DIR/delayed_job.pid
+pgrep -f $RAILS_ENV/delayed_job > $PID_DIR/delayed_job.pid
 
 
