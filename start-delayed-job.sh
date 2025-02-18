75af13bbe2655e43bcb7b78940d27d4bde080967
Gen Schmitt <gschmitt@illinois.edu>
2025-02-18 14:27:45 -0700

    Updated start delayed job to use restart

--- start-delayed-job.sh
+++ start-delayed-job.sh
@@ -4,7 +4,7 @@ source $HOME/bin/set-vars.sh
 
 cd $RAILS_HOME
 bundle install
-bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR start &
+bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR restart &
 sleep 2
 pgrep -f $RAILS_ENV/delayed_job > $PID_DIR/delayed_job.pid
 
