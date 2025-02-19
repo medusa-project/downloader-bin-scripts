8e1ba9bf58162ea22d32aa2755480870b220ddf3
Gen Schmitt <gschmitt@illinois.edu>
2025-02-19 11:38:31 -0700

    Updated start delayed job to start in background

--- start-delayed-job.sh
+++ start-delayed-job.sh
@@ -4,7 +4,7 @@ source $HOME/bin/set-vars.sh
 
 cd $RAILS_HOME
 bundle install
-bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR restart
+bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR restart &
 sleep 15
 pgrep -f $RAILS_ENV/delayed_job | tail -n 1 > $DELAYED_JOB_PID_FILE
 
