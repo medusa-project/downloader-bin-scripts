c97f86c04d22cebb6004178b1604637ab6a52e8d
Gen Schmitt <gschmitt@illinois.edu>
2025-02-19 09:46:43 -0700

    Updated start delayed job to sleep for 15 seconds

--- start-delayed-job.sh
+++ start-delayed-job.sh
@@ -5,7 +5,7 @@ source $HOME/bin/set-vars.sh
 cd $RAILS_HOME
 bundle install
 bundle exec bin/delayed_job -p $RAILS_ENV --pid-dir=$PID_DIR restart
-sleep 10
-pgrep -f $RAILS_ENV/delayed_job | tail -n 1 > $PID_DIR/delayed_job.pid
+sleep 15
+pgrep -f $RAILS_ENV/delayed_job | tail -n 1 > $DELAYED_JOB_PID_FILE
 
 
