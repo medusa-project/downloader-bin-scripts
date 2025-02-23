f7c81db063641b3dfa88d737d0518320dfb97318
Gen Schmitt <gschmitt@illinois.edu>
2025-02-24 09:19:20 +1100

    Updated query-health-check to use mountpoint inplace of rclone

--- query-health-check.rb
+++ query-health-check.rb
@@ -13,9 +13,9 @@ ENV['RAILS_ENV'] == 'demo' ? downloader_uri = URI('https://demo.download.library
 downloader_response = Net::HTTP.get_response(downloader_uri)
 
 begin
-    rclone_monit_status = JSON.parse(downloader_response.body)['rcloneMonitStatus']
-    rclone_mount_status = JSON.parse(downloader_response.body)['rcloneMountStatus']
-    downloader_response_body = {"rclone_monit_status" => rclone_monit_status, "rclone_mount_status" => rclone_mount_status}
+    mountpoint_monit_status = JSON.parse(downloader_response.body)['mountpointMonitStatus']
+    mountpoint_mount_status = JSON.parse(downloader_response.body)['mountpointMountStatus']
+    downloader_response_body = {"mountpoint_monit_status" => mountpoint_monit_status, "mountpoint_mount_status" => mountpoint_mount_status}
 
     downloader_log = {"InstanceId" => instance_id, "downloader_code" => downloader_response.code, "downloader_message" => downloader_response_body}
 rescue JSON::ParserError => e
