2f154f8e71ced1cce4ce8a4157794856cbc59f32
Gen Schmitt <gschmitt@illinois.edu>
2026-06-05 13:53:37 -0600

    Updated health check log to handle amqp and delayed job statuses

--- query-health-check.rb
+++ query-health-check.rb
@@ -7,15 +7,26 @@ require 'json'
 logger = Logger.new('/home/downloader/log/health_check_status.log', 1, 500000)
 
 instance_id = File.open('/var/lib/cloud/data/instance-id', &:readline).strip
-
-ENV['RAILS_ENV'] == 'demo' ? downloader_uri = URI('https://demo.download.library.illinois.edu/downloads/status') : downloader_uri = URI('https://download.library.illinois.edu/downloads/status')
+working_env = ENV['RAILS_ENV']
+working_env == 'demo' ? downloader_uri = URI('https://demo.download.library.illinois.edu/downloads/status') : downloader_uri = URI('https://download.library.illinois.edu/downloads/status')
 
 downloader_response = Net::HTTP.get_response(downloader_uri)
 
 begin
     mountpoint_monit_status = JSON.parse(downloader_response.body)['mountpointMonitStatus']
+    mountpoint_monit_status_hash = {"mountpoint_monit_status" => mountpoint_monit_status}
     mountpoint_mount_status = JSON.parse(downloader_response.body)['mountpointMountStatus']
-    downloader_response_body = {"mountpoint_monit_status" => mountpoint_monit_status, "mountpoint_mount_status" => mountpoint_mount_status}
+    mountpoint_mount_status_hash = {"mountpoint_mount_status" => mountpoint_mount_status}
+    amqp_listener_status = JSON.parse(downloader_response.body)['amqpListener']['status']
+    amqp_listener_status_hash = {"amqp_listener_status" => amqp_listener_status}
+    delayed_job_status = JSON.parse(downloader_response.body)['delayedJobs']['status']
+    delayed_job_status_hash = {"delayed_job_status" => delayed_job_status}
+    
+    downloader_response_body = {}
+    downloader_response_body.merge!(mountpoint_monit_status_hash) unless mountpoint_monit_status.nil?
+    downloader_response_body.merge!(mountpoint_mount_status_hash) unless mountpoint_mount_status.nil?
+    downloader_response_body.merge!(amqp_listener_status_hash) unless amqp_listener_status.nil?
+    downloader_response_body.merge!(delayed_job_status_hash) unless delayed_job_status.nil?
 
     downloader_log = {"InstanceId" => instance_id, "downloader_code" => downloader_response.code, "downloader_message" => downloader_response_body}
 rescue JSON::ParserError => e
